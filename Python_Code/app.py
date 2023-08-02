73730185abbbcb54ead417366484a3329e5a7363
NateKonigkramer214 <nathankonigkramer85@gmail.com>
2023-08-02 15:06:02 +1200

    pyflask task

--- Python_Code/app.py
+++ Python_Code/app.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello World this is braindead why not use HTML</p>"
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/HTML/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/HTML/contact_us.html')
+def contact():
+    return render_template('contact_us.html')
+
+@app.route('/HTML/shop.html')
+def shop():
+    return render_template('shop.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
